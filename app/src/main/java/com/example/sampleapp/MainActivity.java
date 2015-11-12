43b0964219d8b6c901a6a3334bb3e37677f79c0b
donal osullivan <donal osullivan>
2015-11-12 00:27:11 +0000

    First commit

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Declare our view Variables
+        TextView factLabel;
+        Button showFactButton;
+
+    }
+}
